6d79f99c4740f4e6db838ec9dc13014230c0f785
Skander Skope Guarbàa <skander.guarbaa@gmail.com>
2019-02-13 16:56:10 +0100

    Add role validation middleware

---
+++ server/helpers/role-validation.middleware.js
@@ -0,0 +1,23 @@
+const _ = require('lodash');
+const APIError = require('./APIError');
+const httpStatus = require('http-status');
+
+/**
+ * Function validateRoles accepts one role or an array of allowed roles
+ * Return authorization error if user role is not one of the allowed roles
+ */
+module.exports = function validateRoles(roles) {
+  const allowMultipleRoles = _.isArray(roles);
+  return (req, res, next) => {
+    if (allowMultipleRoles) {
+      if (_.includes(roles, req.user.role)) {
+        return next();
+      }
+    }
+    if (roles === req.user.role) {
+      return next();
+    }
+    const err = new APIError('Acces denied permissions required', httpStatus.UNAUTHORIZED, true);
+    return next(err);
+  };
+};
